c8aece75d866d71e349e1bffa295cef927b23eac
yukokondo <yu-kondoh@shoeisha.co.jp>
2015-03-06 11:58:50 +0900

    spec5:外部ファイルをjsonにして、ID付きで表示するようにした

--- recipi.rb
+++ recipi.rb
@@ -1,3 +1,9 @@
-f = open(ARGV[0])
-f.each {|recipi| print recipi}
-f.close
+require 'json'
+require 'open-uri'
+
+recipi_data = open(ARGV[0]).read
+recipi_name = JSON.parse(recipi_data)
+
+recipi_name.each do |list|
+	print list[0] ,":", list[1], "\n" 
+end
