abf80503f8f045e085dbb7dedc77f3afcd4ebf3f
yukokondo <yu-kondoh@shoeisha.co.jp>
2015-03-06 12:17:22 +0900

    spec5:仕様を勘違いしていたので、仕様4の状態に戻した。少し変数名を変えた

--- recipi.rb
+++ recipi.rb
@@ -1,9 +1,3 @@
-require 'json'
-require 'open-uri'
-
-recipi_json = open(ARGV[0]).read
-recipi_name = JSON.parse(recipi_json)
-
-recipi_name.each do |list|
-	print list[0] ,":", list[1], "\n"
-end
+file = open(ARGV[0])
+file.each {|recipi| print recipi}
+file.close
